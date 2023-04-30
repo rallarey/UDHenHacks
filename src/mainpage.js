4f2157edfa4181bc5c67e4abcf74994e87b77625
mathheider <57917849+mathheider@users.noreply.github.com>
2023-04-29 20:01:56 -0400

    commit

--- src/mainpage.js
+++ src/mainpage.js
@@ -3,13 +3,36 @@ let renderPage = (loggedIn)=>{
     var username = '';
     var pic = '';
     let myuid = loggedIn.uid;
-  
+
+    
     
     $("body").html(`
-    <p>hi</p>
+    <p></p>
+    <body> <script>!(function () {
+      let e = document.createElement("script"),
+        t = document.head || document.getElementsByTagName("head")[0];
+      (e.src =
+        "https://cdn.jsdelivr.net/npm/rasa-webchat@1.x.x/lib/index.js"),
+        // Replace 1.x.x with the version that you want
+        (e.async = !0),
+        (e.onload = () => {
+          window.WebChat.default(
+            {
+              initPayload: "/greet",
+              customData: { language: "en" },
+              socketUrl: "http://0.0.0.0:5005",
+              // add other props here
+            },
+            null
+          );
+        }),
+        t.insertBefore(e, t.firstChild);
+    })();
+    </script>
+    </body>
     <button id = "logout">LOG OUT</button>
     `);
-  
+    
   
     $("#logout").on("click", ()=>{
       firebase.auth().signOut();
