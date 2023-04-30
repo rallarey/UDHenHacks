e58e090639bb59082b75a32a8f25a79c479c6173
Ryan Allarey <60989959+rallarey@users.noreply.github.com>
2023-04-29 20:49:12 -0400

    trying to fix merges

--- src/quiz.js
+++ src/quiz.js
@@ -4,6 +4,29 @@ import {renderNextPage} from "./finance.js"
 let renderQuizPage=()=>{
 
   $("body").html(`
+    <p>idk</p>
+        <button id = "budgetpage">BUDGET PAGE</button>
+        <button id = "logout">LOG OUT</button>
+        <script>!(function () {
+            let e = document.createElement("script"),
+              t = document.head || document.getElementsByTagName("head")[0];
+            (e.src =
+              "https://cdn.jsdelivr.net/npm/rasa-webchat@1.x.x/lib/index.js"),
+              // Replace 1.x.x with the version that you want
+              (e.async = !0),
+              (e.onload = () => {
+                window.WebChat.default(
+                  {
+                    customData: { language: "en" },
+                    socketUrl: "http://0.0.0.0:5005",
+                    // add other props here
+                  },
+                  null
+                );
+              }),
+              t.insertBefore(e, t.firstChild);
+          })();
+          </script>
       <nav>
     <ul>
       <li><a href="#">Home</a></li>
