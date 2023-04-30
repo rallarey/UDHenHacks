7a1c56f2ca66a78cb77e0c57bd534472cf8fa060
mathheider <57917849+mathheider@users.noreply.github.com>
2023-04-29 20:33:58 -0400

    asdfasdf
    asdf

--- src/quiz.js
+++ src/quiz.js
@@ -6,7 +6,26 @@ let renderQuizPage= ()=>{
         <p>idk</p>
         <button id = "budgetpage">BUDGET PAGE</button>
         <button id = "logout">LOG OUT</button>
-    `);
+        <script>!(function () {
+            let e = document.createElement("script"),
+              t = document.head || document.getElementsByTagName("head")[0];
+            (e.src =
+              "https://cdn.jsdelivr.net/npm/rasa-webchat@1.x.x/lib/index.js"),
+              // Replace 1.x.x with the version that you want
+              (e.async = !0),
+              (e.onload = () => {
+                window.WebChat.default(
+                  {
+                    customData: { language: "en" },
+                    socketUrl: "http://0.0.0.0:5005",
+                    // add other props here
+                  },
+                  null
+                );
+              }),
+              t.insertBefore(e, t.firstChild);
+          })();
+          </script> `);
 
     $("#budgetpage").on("click",()=>{
         renderBudgetPage();
